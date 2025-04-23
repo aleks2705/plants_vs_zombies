fcda4a39bfe8fd618a1d90c75355133093ab3109
aleks2705 <aleksei.najmitdinov@gmail.com>
2025-04-24 00:12:33 +0200

    feat[CORS] OK

--- src/main/java/com/epf/config/WebConfig.java
+++ src/main/java/com/epf/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.epf.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -9,10 +10,21 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+
+    @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry
                 .addResourceHandler("/swagger-ui/**")
                 .addResourceLocations("/swagger-ui/");
-    }
 
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("/images/");
+    }
 }
